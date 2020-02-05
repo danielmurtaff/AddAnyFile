502462f99173505249e98a4d1b3622fa7db2936c
Daniel Murta <daniel.murta@farfetch.com>
2020-02-05 14:56:19 +0000

    Configs changed

--- src/source.extension.cs
+++ src/source.extension.cs
@@ -8,7 +8,7 @@ namespace MadsKristensen.AddAnyFile
 				static class Vsix
 				{
 								public const string Id = "2E78AA18-E864-4FBB-B8C8-6186FC865DB3";
-								public const string Name = "Add New File";
+								public const string Name = "Add New Migration";
 								public const string Description = @"The fastest and easiest way to add new files to any project - including files that start with a dot";
 								public const string Language = "en-US";
 								public const string Version = "3.5";
